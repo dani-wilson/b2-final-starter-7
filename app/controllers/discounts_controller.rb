2a0251b1b715ea3ad70981bbca0587c76c88fe0b
Dani Wilson <danirae.powell@gmail.com>
2023-08-04 10:06:01 -0600

    add new and create methods with private params

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -6,4 +6,25 @@ class DiscountsController < ApplicationController
   def show
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.bulk_discounts.new(new_discount_params)
+    if @discount.save
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash[:notice] = "Invalid Data - Please Try Again"
+      render :new
+    end
+  end
+
+  private
+
+  def new_discount_params
+    params.permit(:percentage_discount, :quantity_threshold)
+  end
 end
