2147d8b84649608aba51fc81df9df9c817c30c94
Dani Wilson <danirae.powell@gmail.com>
2023-08-08 15:14:56 -0600

    add destroy_all files to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 Rake::Task["csv_load:all"].invoke
 
+BulkDiscount.destroy_all
+InvoiceItem.destroy_all
+Transaction.destroy_all
+Invoice.destroy_all
+Item.destroy_all
+Merchant.destroy_all
+Customer.destroy_all
